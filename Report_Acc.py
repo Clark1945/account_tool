0fb0e06e933f16074adefe70ca834e28e01c3f22
Clark <clark1945@outlook.com>
2023-09-24 22:01:54 +0800

    print 改成寫檔案

--- Report_Acc.py
+++ Report_Acc.py
@@ -18,7 +18,8 @@ def test():
 
     if len(df_today_dist_AC_bank) != 0: distinguish_bank_no_data(df_today_dist_AC_bank, df_yesterday)
     if len(df_today_dist_AC) != 0: distinguish_AC_no_data(df_today_dist_AC, df_yesterday_Bank)
-    distinguish_settlement(df_today_settlement)
+    if len(df_today_settlement) != 0: distinguish_settlement(df_today_settlement)
+
 
 # 判斷AC無資料、結算時間
 def distinguish_AC_no_data(df_today, df_yesterday):
@@ -31,7 +32,7 @@ def distinguish_AC_no_data(df_today, df_yesterday):
     yester_tx_list = sorted(df_yesterday_Bank["交易序號"])
     for x in range(len(today_tx_list)):
         if today_tx_list[x] in yester_tx_list:  # 如果這筆AC無資料 有存在於 昨天的 收單行無資料 且 訂單編號為 1對1對應
-            if (yester_tx_list.count(today_tx_list[x]) == 1):
+            if (yester_tx_list.count(today_tx_list[x]) == 1 and today_tx_list[x] != "Null"):
                 continue
             else:  # 如果 今天的訂單編號在昨天存在多筆，則抓出今天的收單行交易金額，與多筆的同訂單的AC金額比對，True則不印出 ####如果同筆訂單同金額 則會出問題
                 list1 = df_yesterday_Bank[df_yesterday_Bank["交易序號"] == today_tx_list[x]]["AC交易金額"].tolist()
@@ -39,18 +40,20 @@ def distinguish_AC_no_data(df_today, df_yesterday):
                 # if not df_today_dist_AC[df_today_dist_AC["交易序號"] == today_tx_list[x]]["收單行交易金額"].values in list1:
                 #     print(df_yesterday_Bank[df_yesterday_Bank["交易序號"] == today_tx_list[x]])
         else:  # 如果這筆的AC無資料 昨天沒有出現
-            print(df_today_dist_AC[df_today_dist_AC["交易序號"] == today_tx_list[x]])
+            df_today_dist_AC[df_today_dist_AC["交易序號"] == today_tx_list[x]].to_csv("Output.csv", mode="a",
+                                                                                      index=False, header=False)
 
         if yester_tx_list[x] in today_tx_list:  # 如果這筆收單行無資料 有存在於 昨天的 AC無資料 且 訂單編號為 1對1對應
-            if (today_tx_list.count(yester_tx_list[x]) == 1):
+            if (today_tx_list.count(yester_tx_list[x]) == 1 and today_tx_list[x] != "Null"):
                 continue
             else:  # 如果 昨天的訂單編號在今天存在多筆，則抓出昨天的AC交易金額，與多筆的同訂單的AC金額比對，True則不印出
                 list1 = df_today_dist_AC[df_today_dist_AC["交易序號"] == today_tx_list[x]]["收單行交易金額"].tolist()
-                for x in list1: print(x)
+                for x in list1: x.to_csv("Output.csv", mode="a", index=False, header=False)
                 # if not df_yesterday_Bank[df_yesterday_Bank["交易序號"] == today_tx_list[x]]["AC交易金額"].values in list1:
                 #     print(df_today_dist_AC[df_today_dist_AC["交易序號"] == today_tx_list[x]])
         else:  # 如果昨天訂單資料今天沒有
-            print(df_yesterday_Bank[df_yesterday_Bank["交易序號"] == yester_tx_list[x]])
+            df_yesterday_Bank[df_yesterday_Bank["交易序號"] == yester_tx_list[x]].to_csv("Output.csv", mode="a",
+                                                                                         index=False, header=False)
 
 
 # 判斷收單行無資料、結算時間
@@ -72,15 +75,17 @@ def distinguish_bank_no_data(df_today, df_yesterday):
     ubot_time = datetime.datetime(yesterday.year, yesterday.month, yesterday.day, 21, 30, 0)
 
     df_today_dist_ctbc = df_today_dist_ctbc[df_today_dist_ctbc["請款時間"] <= ctbc_time]  # 中信昨晚八點以前的交易
-    if len(df_today_dist_ctbc) != 0: print(df_today_dist_ctbc)
+    if len(df_today_dist_ctbc) != 0: df_today_dist_ctbc.to_csv("Output.csv", mode="a", index=False, header=False)
     df_today_dist_tspg_scrap = df_today_dist_tspg_scrap[
         df_today_dist_tspg_scrap["請款時間"] <= tspg_scrap_time]  # 爬帳昨晚九點以前的交易
-    if len(df_today_dist_tspg_scrap) != 0: print(df_today_dist_tspg_scrap)
+    if len(df_today_dist_tspg_scrap) != 0: df_today_dist_tspg_scrap.to_csv("Output.csv", mode="a", index=False,
+                                                                           header=False)
     df_today_dist_tspg_swp = df_today_dist_tspg_swp[
         df_today_dist_tspg_swp["請款時間"] <= tspg_swp_time]  # SWP 昨晚十點以前的交易
-    if len(df_today_dist_tspg_swp) != 0: print(df_today_dist_tspg_swp)
+    if len(df_today_dist_tspg_swp) != 0: df_today_dist_tspg_swp.to_csv("Output.csv", mode="a", index=False,
+                                                                       header=False)
     df_today_dist_ubot = df_today_dist_ubot[df_today_dist_ubot["請款時間"] <= ubot_time]  # 聯邦九點半以前的交易
-    if len(df_today_dist_ubot) != 0: print(df_today_dist_ubot)
+    if len(df_today_dist_ubot) != 0: df_today_dist_ubot.to_csv("Output.csv", mode="a", index=False, header=False)
 
 
 # 判斷結帳日
@@ -96,6 +101,8 @@ def distinguish_settlement(df_today_settlement):
     df_settlement_abnormal = df_today_settlement[(df_today_settlement["收單行結帳日期"] == settle_time) & (
             (df_today_settlement["AC結帳日期"] != settle_time_tx1) | (
             df_today_settlement["AC結帳日期"] != settle_time_tx2))]
-    if len(df_settlement_abnormal) != 0: print(df_settlement_abnormal)
+    if len(df_settlement_abnormal) != 0: df_settlement_abnormal.to_csv("Output.csv", mode="a", index=False,
+                                                                       header=False)
     df_settlement_exception = df_today_settlement[(df_today_settlement["收單行結帳日期"] != settle_time)]
-    if len(df_settlement_exception) != 0: print(df_settlement_exception)
+    if len(df_settlement_exception) != 0: df_settlement_exception.to_csv("Output.csv", mode="a", index=False,
+                                                                         header=False)
